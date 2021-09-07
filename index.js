61d225b0847da3ff01032f9e5121ab17ea6dc736
MohdAliyan27 <mohd.aliyan.mca@gmail.com>
2021-09-08 01:40:48 +0530

    Resume Files

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app=express();
+
+app.use(express.static('public'))
+
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.resolve(__dirname,'pages/index.html'));
+})
+app.get('/map', (req,res) =>{
+    res.sendFile(path.resolve(__dirname,'pages/map.html'));
+})
+app.listen(4000, () =>{
+    console.log("Server Running at 4000");
+})
